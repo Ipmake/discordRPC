5c49eee7b9c41b4ee3f777258e7250d28333e2d8
Ipmake123 <niko.denis727@gmail.com>
2022-05-20 18:58:44 +0200

    patch

--- main.js
+++ main.js
@@ -21,8 +21,7 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
-      contextIsolation: false,
-      frame: false
+      contextIsolation: false
     }
   })
 
